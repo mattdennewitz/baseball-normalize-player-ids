0b8cc0cc5c2d2ea9b417f859787aeb60c87c79e2
Matt Dennewitz <mattdennewitz@gmail.com>
2016-08-04 15:08:01 -0500

    use six.text_type for setup pip reqs

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ from setuptools import setup, find_packages
 import pip.download
 from pip.req import parse_requirements
 
+import six
+
 
 reqs_txt = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 pip_reqs = parse_requirements(reqs_txt, session=pip.download.PipSession())
-pip_reqs = [unicode(obj.req) for obj in pip_reqs]
+pip_reqs = [six.text_type(obj.req) for obj in pip_reqs]
 
 setup(
     name = 'mlb-normalize-player-ids',
